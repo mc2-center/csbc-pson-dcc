17988217b579d8cb0f193568c36010b24dc6030c
James Eddy <james.a.eddy@gmail.com>
2020-05-14 16:00:23 -0700

    add script to create snapshots for portal tables

---
+++ R/version_bump.R
@@ -0,0 +1,35 @@
+library(reticulate)
+library(dccvalidator)
+library(tidyverse)
+
+# local conda env with Python 3.7 and synapseclient installed
+use_condaenv("csbc-pson-dcc", required = TRUE)
+
+synapseclient <- reticulate::import("synapseclient")
+syn <- synapseclient$Synapse()
+syn$login()
+
+
+bump_table_version <- function(table_id) {
+  timestamp_comment <- lubridate::now(tzone = "UCT") %>% 
+    stringr::str_replace(":[0-9]{2}$", "") %>% 
+    stringr::str_replace(" ", "T")
+
+  snapshot_data <- list(
+    snapshotComment = timestamp_comment
+  )
+  syn$restPOST(
+    glue::glue("/entity/{table_id}/table/snapshot"), 
+    body = jsonlite::toJSON(snapshot_data, auto_unbox = TRUE)
+  )
+}
+
+table_ids <- list(
+  tools = "syn21930566",
+  grants = "syn21918972",
+  datasets = "syn21897968",
+  projects = "syn21868602",
+  publications = "syn21868591"
+)
+
+purrr::walk(table_ids, bump_table_version)
