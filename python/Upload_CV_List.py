0947001662dd72f7cff07d4b7642bfabf91cb896
brynnz22 <brynn.zalmanek@sagebase.org>
2021-03-11 10:47:18 -0800

    add correct copy of script

--- python/Upload_CV_List.py
+++ python/Upload_CV_List.py
@@ -4,28 +4,37 @@ import argparse
 import synapseclient
 from synapseclient import Project, File, Folder
 from synapseclient import Schema, Column, Table, build_table, Row, RowSet, as_table_columns
+from pathlib import Path
 
+# Login to  Synapse
 syn = synapseclient.Synapse()
 syn.login()
 
-# Get arguments
-parser = argparse.ArgumentParser(description='Get synapse project id and filepath')
-parser.add_argument("--p", "--project_id", help="Synapse Project ID")
-# parser.add_argument("--f", "--filepath", help="path to access file")
-
-args=parser.parse_args()
-
-print(args.project_id)
-
-# get project
+# Get Arguments - Synapse project id and file path
+parser = argparse.ArgumentParser(description = 'Get synapse project id')
+parser.add_argument('project_id',type=str,help='Synapse Project ID')
+parser.add_argument('path',type=Path,help='Path to file')
+    
+args = parser.parse_args()
+    
+project_id = args.project_id
+path = args.path
+    
+# get project in Synapse
 project = syn.get(project_id)   
 
 # Create a table from a DataFrame and use only necessary columns
 # Insert filepath, desired column names, and desired name of table
-df = pd.read_csv('/Users/bzalmanek/Desktop/List_Practice.csv', usecols=['key',
-'value','columnType','ontologyId','ontologySource','ontologyUrl'],
-index_col=False)
+df = pd.read_csv(path, 
+    usecols=['key','value','columnType',
+    'ontologyId','ontologySource','ontologyUrl'],
+    index_col=False)
 
 table = build_table('Controlled Vocabulary Master List', project, df)
 
 table = syn.store(table)
+
+print("Table uploaded to Synapse")
+
+
+
