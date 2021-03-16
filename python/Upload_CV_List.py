e76414b2601ee2c347a6f09babf2a8e09cceb38e
brynnz22 <brynn.zalmanek@sagebase.org>
2021-03-16 12:48:55 -0700

    make script a function

--- python/Upload_CV_List.py
+++ python/Upload_CV_List.py
@@ -6,38 +6,43 @@ from synapseclient import Project, File, Folder
 from synapseclient import Schema, Column, Table, build_table, Row, RowSet, as_table_columns
 from pathlib import Path
 
-# Login to  Synapse
-syn = synapseclient.Synapse()
-syn.login()
+def main():
 
-# Get Arguments - Synapse project id and file path
-parser = argparse.ArgumentParser(
-    description='Get synapse project id, file path, and name of table')
-parser.add_argument('project_id', type=str , help='Synapse Project ID')
-parser.add_argument('path', type=Path, help='Path to file')
-parser.add_argument('table_name', type=str, help='Synapse table name')
+    # Login to  Synapse
+    syn = synapseclient.Synapse()
+    syn.login()
+
+    # Get Arguments - Synapse project id and file path
+    parser = argparse.ArgumentParser(
+        description='Get synapse project id, file path, and name of table')
+    parser.add_argument('project_id', type=str , help='Synapse Project ID')
+    parser.add_argument('path', type=Path, help='Path to file')
+    parser.add_argument('table_name', type=str, help='Synapse table name')
     
-args = parser.parse_args()
+    args = parser.parse_args()
     
-project_id = args.project_id
-path = args.path
-table_name = args.table_name
+    project_id = args.project_id
+    path = args.path
+    table_name = args.table_name
     
-# get project in Synapse
-project = syn.get(project_id)   
+    # get project in Synapse
+    project = syn.get(project_id)   
 
-# Create a table from a DataFrame and use only necessary columns
-# Insert filepath and table name
-df = pd.read_csv(path, 
-    usecols=['key', 'value', 'columnType',
-    'ontologyId', 'ontologySource', 'ontologyUrl'],
-    index_col=False)
+    # Create a table from a DataFrame and use only necessary columns
+    # Insert filepath and table name
+    df = pd.read_csv(path, 
+        usecols=['key', 'value', 'columnType',
+        'ontologyId', 'ontologySource', 'ontologyUrl'],
+        index_col=False)
 
-table = build_table(table_name, project, df)
+    table = build_table(table_name, project, df)
 
-table = syn.store(table)
+    table = syn.store(table)
 
-print("Table uploaded to Synapse")
+    print("Table uploaded to Synapse")
+
+if __name__ == "__main__":
+    main()
 
 
 
