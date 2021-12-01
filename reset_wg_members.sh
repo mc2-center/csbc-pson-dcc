ae56679f2975f2c7cbf34569666ec3d5bc6657f8
verena <9377970+vpchung@users.noreply.github.com>
2021-12-01 13:17:01 -0800

    add bash script for cron job

---
+++ reset_wg_members.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Cancer Metabolism WG
+python3 python/reset_team_members.py -t 3387790
+
+# Cell & Tissue Mechanics WG
+python3 python/reset_team_members.py -t 3379962
+
+# CSBC Education & Outreach WG
+python3 python/reset_team_members.py -t 3346987
+
+# PS-ON Education & Outreach WG
+python3 python/reset_team_members.py -t 3346986
+
+# Image Analysis WG
+python3 python/reset_team_members.py -t 3377219
+
+# Resource & Data Sharing WG
+python3 python/reset_team_members.py -t 3346390
