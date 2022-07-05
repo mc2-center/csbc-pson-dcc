e26b4c7e329b3f0dbd103d6b1713f7582689faa4
Verena Chung <9377970+vpchung@users.noreply.github.com>
2022-07-05 09:57:17 -0700

    streamline adding grants (#110)
    * use PAT instead of cached username/password
    
    * update script to utilize new data model and fileview uploaded by schematic

--- python/add_new_grants.py
+++ python/add_new_grants.py
@@ -1,17 +1,13 @@
-"""Add Grants to the Cancer Complexity Knowledge Portal.
+"""Add Grants to the Cancer Complexity Knowledge Portal (CCKP).
 
-This script will iterate through a CSV file (see new_grants_template.csv)
-and create a new Synapse Project and Wiki pages for each listed grant.
-The `Portals - Grants Merged` table will also be updated to include their
-metadata.
-
-TODO:
-  - include step of validating the metadata annotations
-  - auto-add institutions not currently listed in `institutions`?
+This script will iterate through a grants fileview and create a new
+Synapse Project for each new grant not currently in a Grants table.
+Metadata of new grants will also be added to the Grants table.
 
 author: verena.chung
 """
 
+import os
 import argparse
 import getpass
 
@@ -21,64 +17,47 @@ import pandas as pd
 
 
 def login():
-    """Log into Synapse. If cached info not found, prompt user.
+    """Log into Synapse. If env variables not found, prompt user.
 
     Returns:
         syn: Synapse object
     """
-
     try:
-        syn = synapseclient.login(silent=True)
-    except Exception:
-        print("Cached credentials not found; please provide",
-              "your Synapse username and password.")
+        syn = synapseclient.login(
+            authToken=os.getenv('SYNAPSE_AUTH_TOKEN'),
+            silent=True)
+    except synapseclient.core.exceptions.SynapseNoCredentialsError:
+        print("Credentials not found; please manually provide your",
+              "Synapse username and password.")
         username = input("Synapse username: ")
-        password = getpass.getpass("Synapse password: ").encode("utf-8")
+        password = getpass.getpass("Synapse password: ")
         syn = synapseclient.login(username, password, silent=True)
     return syn
 
 
 def get_args():
     """Set up command-line interface and get arguments."""
-
-    parser = argparse.ArgumentParser(
-        description="Add new grants to the CSBC Knowledge Portal.")
-    parser.add_argument("grants_file",
-                        type=str,
-                        help="CSV file of grants")
+    parser = argparse.ArgumentParser(description="Add new grants to the CCKP")
+    parser.add_argument("-m", "--manifest",
+                        type=str, default="syn32134242",
+                        help="Synapse ID to grants manifest fileview. (Default: syn32134242)")
     parser.add_argument("-t", "--grants_table",
                         type=str, default="syn21918972",
-                        help="Add grants to this specified table. "
-                        + "(Default: syn21918972)")
+                        help="Add grants to this specified table. (Default: syn21918972)")
     return parser.parse_args()
 
 
 def create_wiki_pages(syn, project_id, grant_info):
-    """Create Wiki pages for the grant.
-
-    A total of three Wiki pages will be created:
-        - main project page (inc. websites, institutions, abstract, etc.)
-        - `Project Investigators` (list of PIs and their descriptions)
-        - `Data and Tools` (currently empty)
-
-    Assumptions:
-        `piDesc` is already using the Synapse markdown language
-    """
+    """Create main Wiki page for the Project."""
 
     # Main Wiki page
-    consortium = grant_info["consortium"]
-    grant_type = grant_info["grantType"]
-    title = grant_info["institutionAlias"].lstrip(
-        "[").rstrip("]").replace("\"", "")
-    websites = grant_info["website"] or ""
-    institutions = grant_info["grantInstitution"].lstrip(
-        "[").rstrip("]").replace("\"", "")
-    desc = grant_info["abstract"] or ""
+    consortium = grant_info["GrantConsortiumName"]
+    grant_type = grant_info["GrantType"]
+    title = grant_info["GrantInstitutionAlias"]
+    institutions = grant_info["GrantInstitutionName"]
+    desc = grant_info["GrantAbstract"] or ""
 
     content = f"""### The {consortium} {grant_type} Research Project \@ {title}
-
-#### Project Website
-{websites}
 
 #### List of Collaborating Institutions
 {institutions}
@@ -87,41 +66,45 @@ def create_wiki_pages(syn, project_id, grant_info):
 {desc}
 
 """
-    content += "->${buttonlink?text=" + \
-        "Back to CSBC PS%2DON Data Coordinating Center" + \
-        "&url=https%3A%2F%2Fwww%2Esynapse%2Eorg%2F%23%21Synapse%3Asyn7080714%2F}<-"
-    main_wiki = Wiki(title=grant_info["grantName"],
+    content += (
+        "->"
+        "${buttonlink?text="
+        "Back to CSBC PS%2DON Data Coordinating Center"
+        "&url=https%3A%2F%2Fwww%2Esynapse%2Eorg%2F%23%21Synapse%3Asyn7080714%2F}"
+        "<-"
+    )
+    main_wiki = Wiki(title=grant_info["GrantName"],
                      owner=project_id, markdown=content)
     main_wiki = syn.store(main_wiki)
 
     # Sub-wiki page: Project Investigators
-    pis = grant_info["piDesc"]
+    pis = [pi.lstrip(" ").rstrip(" ")
+           for pi
+           in grant_info["GrantInvestigator"].split(",")]
+    pi_markdown = "* " + "\n* ".join(pis)
     pi_wiki = Wiki(title="Project Investigators", owner=project_id,
-                   markdown=pis, parentWikiId=main_wiki.id)
+                   markdown=pi_markdown, parentWikiId=main_wiki.id)
     pi_wiki = syn.store(pi_wiki)
 
-    # Sub-wiki page: Data and Tools
-    data_wiki = Wiki(title="Data and Tools", owner=project_id,
-                     markdown="", parentWikiId=main_wiki.id)
-    data_wiki = syn.store(data_wiki)
 
-
-def add_grants(syn, input_file):
+def create_grant_projects(syn, grants):
     """Create a new Synapse project for each grant and populate its Wiki.
 
     Returns:
         df: grants information (including their new Project IDs)
     """
-
-    grants = pd.read_csv(input_file)
-    grants.dropna(how="all", inplace=True)
-
     for _, row in grants.iterrows():
-        project = Project(row["grantName"])
+        name = row["GrantName"].replace("&", "and")
+        project = Project(name)
         project = syn.store(project)
 
         # Update grants table with new synId
-        grants.loc[_, "grantId"] = project.id
+        grants.at[_, "GrantId"] = project.id
+
+        # Update `GrantId` annotation for ent.
+        annots = syn.get_annotations(row['id'])
+        annots['GrantId'] = project.id
+        syn.set_annotations(annots)
 
         create_wiki_pages(syn, project.id, row)
 
@@ -132,31 +115,56 @@ def upload_metadata(syn, grants, table):
     """Add grants metadata to the Synapse table.
 
     Assumptions:
-        `grants` matches the same schema as `table` (with the exception
-        of two columns; more below)
+        `grants` matches the same schema as `table`
     """
-
-    # `website` and `piDesc` was only needed for the Wiki page creation;
-    # they do not need to be uploaded to the Synapse table.
-    try:
-        grants.drop(columns=['website', 'piDesc'], inplace=True)
-    except KeyError:
-        pass
-
-    # Add annotations to Merged table
     schema = syn.get(table)
+
+    # Reorder columns to match the table order.
+    col_order = [
+        'GrantId', 'GrantName', 'GrantNumber', 'GrantAbstract', 'GrantType',
+        'GrantThemeName', 'GrantInstitutionAlias', 'GrantInstitutionName',
+        'GrantInvestigator', 'GrantConsortiumName'
+    ]
+    grants = grants[col_order]
+
+    # Convert columns into STRINGLIST.
+    grants.loc[:, 'GrantThemeName'] = grants.GrantThemeName.str.split(",")
+    grants.loc[:, 'GrantInstitutionName'] = grants.GrantInstitutionName.str.split(",")
+    grants.loc[:, 'GrantInstitutionAlias'] = grants.GrantInstitutionAlias.str.split(",")
+
+    # Add in missing columns. (TODO: remove?)
+    grants.insert(9, "ConsortiumId", "")
+    grants.insert(6, "InstitutionId", "[]")
+    grants.insert(5, "ThemeId", "")
+
     new_rows = grants.values.tolist()
     table = syn.store(Table(schema, new_rows))
 
 
 def main():
     """Main function."""
-
     syn = login()
     args = get_args()
 
-    grants = add_grants(syn, args.grants_file)
-    upload_metadata(syn, grants, args.grants_table)
+    manifest = syn.tableQuery(f"SELECT * FROM {args.manifest}").asDataFrame()
+    curr_grants = (
+        syn.tableQuery(f"SELECT grantNumber FROM {args.grants_table}")
+        .asDataFrame()
+        .grantNumber
+        .to_list()
+    )
+
+    # Only add grants not currently in the Grants table.
+    new_grants = manifest[~manifest.GrantNumber.isin(curr_grants)]
+
+    if new_grants.empty:
+        print("No new grants found!")
+    else:
+        print(f"{len(new_grants)} new grants found!\nAdding new grants...")
+        #added_grants = create_grant_projects(syn, new_grants)
+        #upload_metadata(syn, added_grants, args.grants_table)
+
+    print("DONE ✓")
 
 
 if __name__ == "__main__":
