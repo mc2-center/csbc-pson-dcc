de79f2c2a71e88b351fe62d41e7ee744a84de199
Verena Chung <9377970+vpchung@users.noreply.github.com>
2022-07-08 02:16:22 -0700

    extract URLs from markdown columns (#114)
    * update column name
    
    * extract the URLs from the markdown
    
    * convert STRINGLIST columns to STRING

--- python/split_merged_tables.py
+++ python/split_merged_tables.py
@@ -36,8 +36,7 @@ def login():
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--type",
-                        type=str, required=True,
+    parser.add_argument("type", type=str,
                         choices=["publication", "dataset",
                                  "tool", "project"],
                         help="Table to split, e.g. `publication`")
@@ -58,11 +57,46 @@ def get_table(syn, args):
     return table
 
 
+def extract_url_columns(table):
+    """Extract just the URLs from columns that contain links in markdown.
+
+    Assumptions:
+        This only applies to the "Dataset Url" column
+    """
+    if "Dataset Url" in table.columns:
+        table["Dataset Url"] = (
+            table["Dataset Url"]
+            .str.extractall(r'\((.*?)\)')
+            .unstack()
+            .fillna("")
+            .apply(", ".join, axis=1)
+            .str.rstrip(", ")
+        )
+    return table
+
+
+def reformat_table(table, colname):
+    """Reformat values in the table."""
+    table = extract_url_columns(table)
+
+    # Find columns in tables that are lists (other than Grant Number) and
+    # convert them to strings.
+    list_cols = (
+        table.columns[table.applymap(type).eq(list).any()]
+        .drop(colname)
+    )
+    table[list_cols] = table[list_cols].applymap(lambda lst: ", ".join(lst))
+    return table
+
+
 def split_table(table, parent):
     """Split table by grant number and output to CSV."""
 
     # Column name for grant numbers depend on the manifest template type.
     colname = f"{parent.capitalize()} Grant Number"
+
+    # Before splitting table, reformat and clean up the table first.
+    table = reformat_table(table, colname)
 
     # Some rows may have multiple grants, so split them up into separate
     # rows so that each row is only associated with one grant number. All
@@ -82,11 +116,12 @@ def split_table(table, parent):
     for grant_number in grouped.groups:
         df = grouped.get_group(grant_number)
 
+        # Only create files if information is found.
         valid_rows = df[~df.applymap(lambda x: len(str(x)) > 500).any(axis=1)]
-        valid_filepath = os.path.join(parent, grant_number + ".csv")
-        valid_rows.to_csv(valid_filepath, index=False)
+        if not valid_rows.empty:
+            valid_filepath = os.path.join(parent, grant_number + ".csv")
+            valid_rows.to_csv(valid_filepath, index=False)
 
-        # Only create a file if there are invalid rows found.
         invalid_rows = df[df.applymap(lambda x: len(str(x)) >= 500).any(axis=1)]
         if not invalid_rows.empty:
             invalid_filepath = os.path.join(
